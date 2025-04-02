f633f7b7aaaecead85a4324c630a3fa777bccafc
ROOBAN <roopavanan1009@gmail.com>
2025-04-02 17:33:32 +0530

    first commit

---
+++ _next/static/chunks/5e22fd23-092f2eee8ffc9cb4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[206],{748:(t,a,e)=>{e.d(a,{q2p:()=>h});var c=e(4436);function h(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M128 192l128 128 128-128z"},child:[]}]})(t)}}}]);
